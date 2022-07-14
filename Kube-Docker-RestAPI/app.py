b5f3da956291caa33dfcc860c74d3f2c2f3917a5
RASHI KASHYAP <kashyapr78@gmail.com>
2022-07-14 19:40:53 +0530

    Kube-docker-RestAPI

---
+++ Kube-Docker-RestAPI/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+# from flask import Flask, redirect, url_for, render_template, request
+# app = Flask(__name__)
+
+# @app.route('/success/<name>')
+# def success(name):
+#    return 'Welcome %s' % name
+
+# @app.route('/login',methods = ['POST', 'GET'])
+# def login():
+#    if request.method == 'POST':
+#       user = request.form['nm']
+#       return redirect(url_for('success',name = user))
+#    else:
+#       user = request.args.get('nm')
+#       return redirect(url_for('success',name = user))
+
+# if __name__ == '__main__':
+#    app.run(debug = True)
