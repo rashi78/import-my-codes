7a62242b258f8f91ee3d0c384eefc767120a922b
Rashi Kashyap <43238406+rashi78@users.noreply.github.com>
2022-07-14 19:45:37 +0530

    Update app.py

--- Kube-Docker-RestAPI/app.py
+++ Kube-Docker-RestAPI/app.py
@@ -20,29 +20,3 @@ def user(usr):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-# from flask import Flask, redirect, url_for, render_template, request
-# app = Flask(__name__)
-
-# @app.route('/success/<name>')
-# def success(name):
-#    return 'Welcome %s' % name
-
-# @app.route('/login',methods = ['POST', 'GET'])
-# def login():
-#    if request.method == 'POST':
-#       user = request.form['nm']
-#       return redirect(url_for('success',name = user))
-#    else:
-#       user = request.args.get('nm')
-#       return redirect(url_for('success',name = user))
-
-# if __name__ == '__main__':
-#    app.run(debug = True)
